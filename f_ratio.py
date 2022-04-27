070a8c0af6d310cb1ecc8c0b6d65244d21e0745d
Maciej Burzyński <90728766+maciekbski@users.noreply.github.com>
2022-04-27 23:01:11 +0200

    docstring added, equations corrected

--- f_ratio.py
+++ f_ratio.py
@@ -6,12 +6,16 @@ n = -0.93
 p = 0.6
 m = 0.49
 
-def calculate_f(q, s, params):
-	if(q < params[1]):
-		f = params[0] * ((q) ** (params[3])) * (s ** params[4])
-	else:
-		f = params[0] * ((q) ** (params[3])) * (s ** params[4])
-	return f
+def calculate_f(s, q, params):
+    """
+    this function calculates f(s, q, A, q_br, n, p, m) = A*s**m*[(q/q_br)**n*theta(q-q_br)+(q/q_br)**p*theta(q_br-q)]
+    where theta is the Heaviside function
+    """
+    if(q > params[1]):
+        f = params[0] * ((q/params[1]) ** (params[2])) * (s ** params[4])
+    else:
+        f = params[0] * ((q/params[1]) ** (params[3])) * (s ** params[4])
+    return f
 
 params = (A, q_br, n, p, m)
 (q, s) = np.loadtxt("sq_dgen.dat", usecols = (1, 2), unpack = True)
@@ -22,12 +26,10 @@ ratio = np.zeros((int(len(q) / 2), 1))
 k = 0
 	
 for i in range(0, len(q)):	
-	f[i] = calculate_f(q[i], s[i], params)
-	#f[i+1] = calculate_f(q[i+1], s[i+1], params)
-	#print(f[i])
-	
+    f[i] = calculate_f(s[i], q[i], params)
+
 for j in range(0, int(len(q) / 2)):
-	ratio[j] = f[k] / f[k+1]
-	k += 2
-	print(ratio[j])
+    ratio[j] = f[k] / f[k+1]
+    print(*ratio[j])
+    k += 2
 	
