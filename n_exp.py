8b7bdbc338182c1c32a8d2598242cc21d7369e2b
Jakub Suchecki <kuba@leomsi.localdomain>
2022-05-09 12:53:25 +0200

    Fixed bugs in fun_massratio & moved "A" to get_N_exp

--- n_exp.py
+++ n_exp.py
@@ -14,13 +14,13 @@ def get_lower_nearest_index(array, x):
 def fun_massratio(s, q, q_break, params): 
     """
     generating mass-ratio function in shape of f(s, q, params = [A,q_break,n,p,m]) = A*[(q/q_break)^n+(q/q_break)^p]*s^m 
-    params = [A, q_break, n, p, m]
+    params = [q_break, n, p, m]
     """
-    A, n, p, m = params
-    if q < params[1]:        
-        return A*(q/q_break)**n*s**m
+    n, p, m = params
+    if q > q_break:        
+        return (q/q_break)**n*s**m
     else:
-        return A*(q/q_break)**p*s**m
+        return (q/q_break)**p*s**m
 
 def fun_sensitivity(s,q, data):
     """
@@ -48,19 +48,20 @@ def fun_sensitivity(s,q, data):
 #    print([np.log10(q), np.log10(s), interpolated_surv_sens])
     return interpolated_surv_sens
 
-def get_N_exp(params, q_break, data): 
+def get_N_exp(params, q_break, data, Aqr): 
     """
     Trivial integral by summation through all data given of function f*S where f is mass-ration function, S is survey sensitivity given by data
     params = [A, q_break, n, p, m]
-    data = [log_s, log_q, survey_sensitivity] 
+    data = [log_s, log_q, survey_sensitivity]
+    Aqr = A constant in 
     """
     integral = 0
     log_s, log_q, surv_sens = data
     for i in range(len(log_q)-1):
         for j in range(len(log_s)-1):
-            dsdq = (log_s[i]-log_s[i+1])*(log_q[i]-log_q[i+1])
+            dsdq = (log_s[i+1]-log_s[i])*(log_q[i+1]-log_q[i])
             integral += dsdq*fun_massratio(10**log_s[i],10**log_q[i], q_break, params)*surv_sens[i,j]
-    return integral
+    return Aqr*integral
 
 sensitivity_data = pd.read_csv('data/survey_sensitivity2.dat', header=None)
 data = sensitivity_data.to_numpy()
@@ -72,6 +73,6 @@ q_break = 1.7e-4
 n = -0.92
 p = 0.47
 m = 0.5
-params = [A, n, p, m]
-N_exp = get_N_exp(params, q_break, [log_s, log_q, surv_sens])
+params = [n, p, m]
+N_exp = get_N_exp(params, q_break, [log_s, log_q, surv_sens], A)
 print(N_exp)
