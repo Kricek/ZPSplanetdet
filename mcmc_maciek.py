2d3b5f809e919057e0ad5f745849f393b5209ea1
Maciej Burzyński <90728766+maciekbski@users.noreply.github.com>
2022-07-21 19:50:49 +0200

    changes in get_log_likelihood(), calculate_S added

--- mcmc_maciek.py
+++ mcmc_maciek.py
@@ -9,20 +9,20 @@ from tqdm import tqdm
 """
 step by step analysis as described in Suzuki+16
 pdf - probability density function
+modyfikowana wersja mcmc_maciek.py z githuba
 """
 
-def get_log_likelihood(params, q_break, data):
+def get_log_likelihood(params, q_break, S, sensitivity_data, planet_data):
     """
     pdf for the data given the parameters
     """
-    log_s, log_q, surv_sens = data 
+    log_s, log_q, surv_sens = sensitivity_data 
+    s, q = planet_data
     A, m, n, p = params
-    S = 0 # survey sensitivity
-    log_likelihood = -1 * get_N_exp(params, q_break, data) 
-    for i in range(len(log_q)):
-        for j in range(len(log_s)):    
-            log_likelihood += np.log(A) + np.log(get_massratio(params, q_break, 10**log_s[j], 10**log_q[i]))
-            S += surv_sens[i][j] 
+    #nexp = get_N_exp(params, q_break, data)
+    log_likelihood = -1 * get_N_exp(params, q_break, sensitivity_data) 
+    for i in range(len(log_q)):    
+        log_likelihood += np.log(A) + np.log(get_massratio(params, q_break, s[i], q[i]))
     return log_likelihood + np.log(S)
 
 def get_log_prior(params):
@@ -34,17 +34,28 @@ def get_log_prior(params):
         return 0.0
     return -np.inf
 
-def get_log_probability(params, q_break, data):
+def get_log_probability(params, q_break, S, sensitivity_data, planet_data):
     """
     posterior pdf for parameters given the data to be sampled
     """
-    log_s, log_q, surv_sens = data
+    log_s, log_q, surv_sens = sensitivity_data
+    s, q = planet_data
     lp = get_log_prior(params)
     if not np.isfinite(lp):
         return -np.inf
-    return lp + get_log_likelihood(params, q_break, data)
+    return lp + get_log_likelihood(params, q_break, S, sensitivity_data, planet_data)
+    
+def calculate_S(data):
+    log_s, log_q, surv_sens = data
+    (max_j, max_i) = (surv_sens.shape) 
+    S = 0
+    for i in range(max_j):
+        for j in range(max_j):    
+            S += surv_sens[i][j]
+    return S
 
-#values imported from Suzuki+16
+
+# values imported from Suzuki+16
 
 A = 0.61
 m = 0.5
@@ -53,26 +64,39 @@ p = 0.44
 q_break = 1.7e-4
 params = [A, m, n, p]
 
-#reading data from survey_sensitivity2.dat
+# reading data from survey_sensitivity2.dat
 
 sensitivity_data = pd.read_csv('data/survey_sensitivity2.dat', header=None)
-data = sensitivity_data.to_numpy()
-log_s = data[0,2:]
-log_q = data[1:,1]
-surv_sens = data[1:,2:]
-surv_data = [log_s, log_q, surv_sens]
+sensitivity_data = sensitivity_data.to_numpy()
+log_s = sensitivity_data[0,2:]
+log_q = sensitivity_data[1:,1]
+surv_sens = sensitivity_data[1:,2:]
+sensitivity_data = [log_s, log_q, surv_sens]
 
-#setting starting positions for walkers
+# reading data from planets_data.dat
+
+planet_data = pd.read_csv('data/planets_data.csv', header=None)
+planet_data = planet_data.to_numpy()
+s = planet_data[0:,2]
+q = planet_data[0:,1]
+q *= 10**3
+planet_data = [s, q]
+
+# calculating S
+
+S = calculate_S(sensitivity_data)
+
+# setting starting positions for walkers
 
 pos = np.random.rand(15, 4)
 nwalkers, ndim = pos.shape
 
-#running emcee
+# running emcee
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, get_log_probability, args = (q_break, surv_data))
+sampler = emcee.EnsembleSampler(nwalkers, ndim, get_log_probability, args = (q_break, S, sensitivity_data, planet_data))
 sampler.run_mcmc(pos, 1000, progress=True)
 
-#visualization of results
+# visualization of results
 
 labels = ["A", "m", "n", "p"]
 flat_samples = sampler.get_chain(flat = True)
