a83bacc47b645235cc3814309b4ef2ccb6fc2f9f
maciekbski <maciek.bski@gmail.com>
2022-09-23 15:49:42 +0200

    changes in visualization of the results

--- mcmc_degen_plus.py
+++ mcmc_degen_plus.py
@@ -22,8 +22,10 @@ def get_log_likelihood(params, q_break, sensitivity_data, planet_data, S):
     s, q, w = planet_data
     A, m, n, p, x_4, x_19, x_22, x_24 = params
     model_params = (A, m, n, p)
-    log_L0 = -1 * get_N_exp(model_params, q_break, sensitivity_data)
     i = 0
+    
+    log_L0 = -1 * get_N_exp(model_params, q_break, sensitivity_data)
+    
     while i < len(q):        
         if(w[i] == 1):    
             log_L0 += np.log(A) + np.log(get_massratio(model_params, q_break, s[i], q[i])) + np.log(S[i])
@@ -79,10 +81,12 @@ m = 0.5
 n = -0.92
 p = 0.44
 q_break = 1.7e-4
+
 x_4 = 0.5
 x_19 = 0.5
 x_22 = 0.5
 x_24 = 0.5
+
 params = [A, m, n, p, x_4, x_19, x_22, x_24]
 
 # reading data from survey_sensitivity2.dat
@@ -124,11 +128,57 @@ pos_new = sampler.run_mcmc(pos, 400, progress=True)
 sampler.reset()
 sampler.run_mcmc(pos_new, 600, progress=True)
 
-# visualization of results
 
-labels = ["A", "m", "n", "p", "x_4", "x_19", "x_22", "x_24"]
+# obtaining the flat samples
+
 flat_samples = sampler.get_chain(flat = True)
-figure = corner.corner(flat_samples, labels=labels,
-                       quantiles=[0.16, 0.5, 0.84],
-                       show_titles=True, title_kwargs={"fontsize": 12})
+A_plot, m_plot, n_plot, p_plot, x4_plot, x19_plot, x22_plot, x24_plot = flat_samples.T
+
+# printing the fraction of values above/below the limit
+
+filtr1 = (x4_plot < w[5])
+filtr2 = (x19_plot < w[20])
+filtr3 = (x22_plot < w[22])
+filtr4 = (x24_plot < w[24])
+
+x4_below = x4_plot[filtr1]
+x4_above = x4_plot[~filtr1]
+
+x19_below = x19_plot[filtr2]
+x19_above = x19_plot[~filtr2]
+
+x22_below = x22_plot[filtr3]
+x22_above = x22_plot[~filtr3]
+
+x24_below = x24_plot[filtr4]
+x24_above = x24_plot[~filtr4]
+
+print("x4<", w[5], ": ", len(x4_below),"/",len(x4_plot), sep='')
+print("x4>", w[5], ": ", len(x4_above),"/",len(x4_plot), sep='')
+
+print("x19<", w[20], ": ", len(x19_below),"/",len(x4_plot), sep='')
+print("x19>", w[20], ": ", len(x19_above),"/",len(x4_plot), sep='')
+
+print("x22<", w[22], ": ", len(x22_below),"/",len(x22_plot), sep='')
+print("x22>", w[22], ": ", len(x22_above),"/",len(x22_plot), sep='')
+
+print("x24<", w[24], ": ", len(x24_below),"/",len(x24_plot), sep='')
+print("x24>", w[24], ": ", len(x24_above),"/",len(x24_plot), sep='')
+
+# visualization of the results
+
+labels1 = ["A", "m", "n", "p"]
+labels2 = ["x4_below"]
+labels3 = ["x4_above"]
+
+results = np.array([A_plot, m_plot, n_plot, p_plot])#_below, x4_above])
+results = results.T
+
+figure = corner.corner(results, labels=labels1, quantiles=[0.16, 0.5, 0.84],              show_titles=True, title_kwargs={"fontsize": 12})
+
+figure = corner.corner(x4_below.T, labels=labels2, color="blue", show_titles=True, title_kwargs={"fontsize": 12})#, fig = figure)
+                                   
+figure = corner.corner(x4_above.T, labels=labels3, color="red", show_titles=True, title_kwargs={"fontsize": 12})#, fig = figure2)
+                          
 plt.show()
+
